4601eaadd48fd913e6a5e3c7fd8d445794758832
georgesmbock <georgesmbo89@gmail.com>
2023-05-23 16:43:46 -0700

    0. _putchar and main

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ *putchar -Entry
+ *Description:This programm prints _putchar
+ *Return:Alwyas 0
+ */
+int putchar(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
