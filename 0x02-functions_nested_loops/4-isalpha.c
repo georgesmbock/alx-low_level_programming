b8772b0e0dbacbe96cbde6cd8b44f37c8fccf994
georgesmbock <georgesmbo89@gmail.com>
2023-05-24 10:51:39 -0700

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,7 +7,9 @@
  */
 int _isalpha(int c)
 {
-	if ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z')
+	if ('a' <= c && c <= 'z')
+	       return (1);
+	if ('A' <= c && c <= 'Z')
 		return (1);
 	return (0);
 }
