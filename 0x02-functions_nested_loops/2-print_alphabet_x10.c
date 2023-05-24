423df29a07f845f41b1e4d1fb35de439bea37ca3
georgesmbock <georgesmbo89@gmail.com>
2023-05-24 10:15:37 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,8 +10,8 @@ void print_alphabet_x10(void)
 	int i;
 	int d;
 
-	for (d = 'a'; d <= 'z'; d++)
-		for (i = 0; i <= 10; i++)
+	for (i = 'a'; i <= 'z'; i++)
+		for (d = 'a'; d <= 'z'; d++)
 			_putchar(d);
 	_putchar('\n');
 }
