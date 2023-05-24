18284e08b0b1c18fb8b605fb49159f92a3fc21c8
georgesmbock <georgesmbo89@gmail.com>
2023-05-24 16:50:00 -0700

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *times_table - print a times x
+ *-
+ *Return:nothing
+ */
+void times_table(void)
+{
+	int x, y, z;
+
+	while (x <= 9)
+	{
+		y = 0;
+		while (y <= 9)
+		{
+			z = x * y;
+			if (y == 0)
+			{
+				_putchar(z + '0');
+			}
+			else if (z < 10)
+			{
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+			{
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+			if (y < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
