4b64b0b16c51502ec0d19d31655f92925bc64ba0
georgesmbock <georgesmbo89@gmail.com>
2023-05-24 10:49:27 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_isalpha - checks for alphabetic character
+ *@c:integer
+ *Return:1 if c is letter lower or upp and 0 else
+ */
+int _isalpha(int c)
+{
+	if ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z')
+		return (1);
+	return (0);
+}
