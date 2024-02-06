456ab07283e932ae07641a0172c2f527250a6bdf
georgesmbock <georgesmbo89@gmail.com>
2024-02-06 04:14:50 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	fprintf($CFILE, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
