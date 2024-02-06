f1b01e55d72ac5444d22397af5c5c68a9a11e152
georgesmbock <georgesmbo89@gmail.com>
2024-02-06 04:11:55 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	fprintf(stdout, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+	fprintf(, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
