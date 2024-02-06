f1c1c0681cecbac9641321dbbac95d23af622406
georgesmbock <georgesmbo89@gmail.com>
2024-02-05 22:30:49 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/**
+ * main - function
+ * -
+ *  Return:0
+ */
+int main(void)
+{
+	char s[100] = "/"Programming is like building a multilingual puzzle";
+		puts(s);
+		return (0);
+}
