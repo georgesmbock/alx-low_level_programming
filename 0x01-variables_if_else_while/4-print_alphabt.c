aa5d70f7eb9b3d8df4bb7e0519c27bc29210a0af
georgesmbock <georgesmbo89@gmail.com>
2024-02-09 04:17:41 -0800

    4-print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,11 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'Z'; c++)
-	{
-		if (c != 'e') && (c != 'q')
+		if ((c != 'e') && (c != 'q'))
 		{
 			putchar(c);
 		}
-	}
+
 	putchar('\n');
 	return (0);
 }
