42861af36bed442a4fcdb408411fca20813c4c93
georgesmbock <georgesmbo89@gmail.com>
2024-03-04 18:23:41 -0800

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * -
+ * Return:Nothing
+ */
+void main(void)
+{
+	int n;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i + '\0');
+	for (i = 'a'; t < 'g'; i++)
+		putchar(i + '\0');
+	putchar('\n');
+}
