72bdd7a2fcd077735a31fdee860676c0a2008685
georgesmbock <georgesmbo89@gmail.com>
2024-03-07 09:28:28 -0800

    101 print comb4

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,27 +8,24 @@
  */
 int main(void)
 {
-	int x = 0;
-	int y = 0;
+	int x;
+	int y;
 
-	for (x = 0; x <= 99; x++)
+	for (x = 0; x <= 98; x++)
 	{
-		for (y = 0; y <= 99; y++)
+		for (y = x + 1; y <= 99; y++)
 		{
-			if (x < y && x != y)
-			{
-				putchar((x / 10) + '0');
-				putchar((x % 10) + '0');
-				putchar(' ');
-				putchar((y / 10) + '0');
-				putchar((y % 10) + '0');
-				if (x != 98 || y != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(' ');
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+			if (x == 98 && y == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
