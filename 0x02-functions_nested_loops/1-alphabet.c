729d6a31792329bfe6c810433e9a5b95c4865fb8
georgesmbock <georgesmbo89@gmail.com>
2024-03-07 13:16:46 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -Entry point
+ * -
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	return (0);
+}
